160d74c917b98e43cc1e94a299d98884abafa5b0
Daniel Lamando <apt@danopia.net>
2018-04-21 21:49:33 -0700

    Implement mustache layouts, and posts

--- blog-generator/index.js
+++ blog-generator/index.js
@@ -1,5 +1,8 @@
 const Future = require('fibers/future');
 const {markdown} = require('markdown');
+const Mustache = require('mustache');
+const moment = require('moment');
+const {basename} = require('path');
 
 const {StartEnvClient, StartClient} = require('../nodejs-domain-client');
 Future.task(() => {
@@ -9,46 +12,106 @@ Future.task(() => {
   profile = StartEnvClient('blog').wait();
   hosting = StartClient('devmode.cloud', 'public', '', 'blog').wait();
 
-  console.log('Generating blog...');
-  const config = profile.loadDataStructure('/config/blog').wait();
+  console.log('Loading blog configuration...');
+  const config = profile.loadDataStructure('/config/blog', 3).wait();
   const siteTitle = config.siteTitle || 'New Blog';
+  const siteSubtitle = config.siteSubtitle || 'Content goes here';
 
-  const htmlFiles = [];
-  let indexHtml = `<!DOCTYPE html>
-    <title>${siteTitle}</title>
-    <h1>${siteTitle}</h1>
-    <h3>Posts</h3>
-    <ul>`;
+  // load all the layout HTML files into a Map
+  const layouts = new Map;
+  Object.keys(config.layouts).forEach(fileName =>
+    layouts.set(basename(fileName, '.html'),
+      config.layouts[fileName].load().wait()));
 
-  profile.listChildNames('/persist/blog/pages').wait().forEach(slug => {
-    const page = profile.loadDataStructure('/persist/blog/pages/'+slug, 2).wait();
-    const pageTitle = page.title || 'Untitled Page';
-    // add page to homepage nav
-    indexHtml += `<li><a href="${slug}.html">${page.title}</a></li>\n`;
+  console.log('Loading pages and posts...');
 
-    let innerHtml = '';
-    if (page.html) {
-      innerHtml = page.html.load().wait();
-    } else if (page.markdown) {
-      const pageSource = page.markdown.load().wait();
-      innerHtml = markdown.toHTML(pageSource);
+  function renderInnerHtml(content) {
+    if (content.html) {
+      return content.html.load().wait();
+    } else if (content.markdown) {
+      return markdown.toHTML(content.markdown.load().wait());
     }
+    throw new Error("No innerHtml for content");
+  }
 
-    htmlFiles.push({
-      path: `/${slug}.html`,
-      body: `<!DOCTYPE html>
-        <title>${page.title} - ${siteTitle}</title>
-        <h2><a href=".">${siteTitle}</a></h2>
-        <h1>${page.title}</h1>
-        <div id="page-body">${innerHtml}</div>`});
+  function loadContentNodes(path) {
+    return profile.listChildNames(path).wait().map(slug => {
+      const data = profile.loadDataStructure(path+'/'+slug, 2).wait();
+      return {
+        path: `${slug}.html`,
+        title: data.title || slug,
+        innerHtml: renderInnerHtml(data),
+        raw: data,
+      };
+    });
+  }
+  const pages = loadContentNodes('/persist/blog/pages');
+  const posts = loadContentNodes('/persist/blog/posts');
+
+  posts.forEach(p => {
+    const publishedAt = moment(p.raw.publishedAt);
+    if (publishedAt.isValid()) {
+      p.publishDate = publishedAt.format('LL [at] LT');
+      p.path = `posts/${publishedAt.format('YYYY')}/${p.path}`;
+    }
   });
 
-  indexHtml += `</ul>`;
-  htmlFiles.push({path: '/index.html', body: indexHtml});
+  console.log('Generating blog files...');
+
+  // helper to pass a data object though one layout, then the site layout
+  // special page? don't pass a layout, pass html as data.innerHtml instead
+  function renderPage(data, layout) {
+    var {innerHtml, baseHref} = data;
+    if (layouts.has(layout)) {
+      innerHtml = Mustache.render(layouts.get(layout), data);
+    }
+    if (!innerHtml) throw new Error("No innerHtml for content");
+
+    return Mustache.render(layouts.get('default'), {
+      siteTitle, siteSubtitle,
+      pages, posts,
+      innerHtml, baseHref,
+    });
+  }
+
+  function reversePath(path) {
+    if (path.includes('/')) {
+      return path.split('/').slice(1).map(x => '..').join('/');
+    } else {
+      return '.';
+    }
+  }
+
+  const htmlFiles = [];
+  function renderContentNodes(list, layout) {
+    list.forEach(content => {
+      content.baseHref = reversePath(content.path);
+      htmlFiles.push({
+        path: '/'+content.path,
+        body: renderPage(content, layout),
+      });
+    });
+  }
+  renderContentNodes(pages, 'page');
+  renderContentNodes(posts, 'post');
+
+  htmlFiles.push({
+    path: '/index.html',
+    body: renderPage({
+      posts, pages,
+    }, 'home'),
+  });
 
   console.log('Uploading', htmlFiles.length, 'HTML files to web hosting...');
   htmlFiles.forEach(({path, body}) => {
     hosting.callApi('putFile', '/web/blog'+path, body).wait();
+  });
+
+  const assetKeys = Object.keys(config.assets);
+  console.log('Uploading', assetKeys.length, 'site assets...');
+  assetKeys.forEach(asset => {
+    const body = config.assets[asset].load().wait();
+    hosting.callApi('putFile', '/web/blog'+'/'+asset, body).wait();
   });
 
   const endTime = new Date();
